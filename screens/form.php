455fe7ac8f4b689ac6b2a2d43b522873d13dabc8
Scarsan <kevinj@peraltadesign.com>
2014-02-19 19:06:28 -0500

    Update form.php

--- screens/form.php
+++ screens/form.php
@@ -1,18 +1,14 @@
 <form id="gantry-import-export" action="" method="POST" enctype="multipart/form-data">
-	<table style="width: 100%">
-		<tr valign="top">
-			<td style="width: 50%">
-				<h4>Import Settings</h4>
-				<input type="file" name="file" />
-				<input type="submit" name="upload" id="upload" class="button-secondary" value=" Upload ">
-			</td>
-			<td>
-				<h4>Export Settings</h4>
-				<a href="<?php echo admin_url('admin.php?page=gantry-theme-settings&action=download'); ?>" class="button-secondary">Download as file</a>
-			</td>
-		</tr>
-	</table>
+<div class="custom-gantry-import-export-spacer">
+<h4>Import Settings</h4>
+<input type="file" name="file" />
+<input type="submit" name="upload" id="upload" class="button-secondary" value=" Upload ">
+</div>
+
+<div class="custom-gantry-import-export-spacer">
+<h4>Export Settings</h4>
+<a href="<?php echo admin_url('admin.php?page=gantry-theme-settings&action=download'); ?>" class="button-secondary">Download as file</a>
+</div>
 </form>
-<script>
-	jQuery('#gantry-import-export').insertAfter('#g4-panels').show();
-</script>
+
+<script> jQuery('#gantry-import-export').insertAfter('#g4-container').show(); </script>
